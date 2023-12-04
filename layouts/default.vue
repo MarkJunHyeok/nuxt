13b08be91080b4cfdc2190f7352ad3fe1e04af43
Mark <junheak65@xingsoft.co.kr>
2023-12-04 14:34:59 +0900

    공통 레이아웃 설정

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <nav>네비게이션</nav>
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- pages/About.vue
+++ pages/About.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import {definePageMeta} from "#imports";
 
+definePageMeta({
+  layout: false
+})
 </script>
 
 <template>
