311fd5ed530a6bab85dc9553d1f668c3af970987
Mark <junheak65@xingsoft.co.kr>
2023-12-04 14:55:58 +0900

    동적 layout 적용

---
+++ pages/DynamicLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <button @click="enableCustomLayout">Update layout</button>
+  </div>
+</template>
+<script setup lang="ts">
+const route = useRoute();
+const enableCustomLayout = () => {
+  route.meta.layout = 'default';
+};
+definePageMeta({
+  layout: false,
+});
+</script>
