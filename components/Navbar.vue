0eec85cd20867334843b7498df9bc1c71844a429
Mark <junheak65@xingsoft.co.kr>
2023-12-04 15:57:13 +0900

    계층 페이지 작성 및 페이지의 네스트화

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,6 +2,7 @@
   <nav>
     <NuxtLink to="/">Home</NuxtLink><br>
     <NuxtLink to="/about">About</NuxtLink><br>
-    <NuxtLink to="https://google.com" target="_blank">Google</NuxtLink>
+    <NuxtLink to="https://google.com" target="_blank">Google</NuxtLink><br>
+    <NuxtLink :to="{name : 'users-list' }" >User List</NuxtLink>
   </nav>
 </template>
---
+++ pages/users.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1>Users</h1>
+    <NuxtPage/>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
