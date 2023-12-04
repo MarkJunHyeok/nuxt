8f2f7c564b312f6e9dcb304055fede2e9ed1c7a1
Mark <junheak65@xingsoft.co.kr>
2023-12-04 17:03:46 +0900

    useAsyncData

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,20 @@
 <script setup lang="ts">
+import type {Ref} from "vue";
+
 const { count, inc, dec} = useCounter(100)
 const { $hello } = useNuxtApp()
+
+export interface Post{
+  id: number,
+  title: string,
+  body: string
+}
+
+const { data: posts, error, pending } = await useAsyncData<Post[]>('posts', () =>
+    $fetch('https://jsonplaceholder.typicode.com/posts/')
+);
+
+
 </script>
 
 <template>
@@ -16,6 +30,14 @@ const { $hello } = useNuxtApp()
   <div>
     {{$hello('World')}}
   </div>
+
+  <p v-if="error">{{ error }}</p>
+  <p v-if="pending">Loading....</p>
+  <ul>
+    <li v-for="post in posts!" :key="post.id">
+      <NuxtLink :to="`/posts/${post.id}`">{{ post.title }}</NuxtLink>
+    </li>
+  </ul>
 </template>
 
 <style>
---
+++ pages/posts/[id].vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type {Post} from "~/pages/index.vue";
+import type {Ref} from "vue";
+
+const route = useRoute()
+
+console.log(route.params.id)
+
+const { data: post } : { data: Ref<Post> } = await useAsyncData('posts', () =>
+    $fetch(`https://jsonplaceholder.typicode.com/posts/${route.params.id}`)
+);
+
+
+</script>
+
+<template>
+  <p>ID: {{ route.params.id }}</p>
+  <p>Title: {{post.title}}</p>
+  <p>Content: {{post.body}}</p>
+</template>
+
+<style scoped>
+
+</style>
