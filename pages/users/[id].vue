df8c607a86a7fa9fb411be046d5638596b9b0b98
Mark <junheak65@xingsoft.co.kr>
2023-12-04 16:00:29 +0900

    다이나믹 라우팅

---
+++ pages/users/[id].vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const route = useRoute()
+
+console.log(route.params.id)
+
+</script>
+
+<template>
+  <p>ID: {{ route.params.id }}</p>
+</template>
+
+<style scoped>
+
+</style>
