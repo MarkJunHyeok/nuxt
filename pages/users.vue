9b7ecf41f60ce78b1e25f24fde8c00b9453cb1a9
Mark <junheak65@xingsoft.co.kr>
2023-12-05 10:08:31 +0900

    createError

--- pages/users.vue
+++ pages/users.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
+const resetError = (error) => {
+  error.value = null
+}
 
+const someErrorLogger = (error) => {
+  console.log('someErrorLogger', error)
+}
 </script>
 
 <template>
   <div>
     <h1>Users</h1>
-    <NuxtPage/>
+    <NuxtErrorBoundary @error="someErrorLogger">
+      <NuxtPage/>
+      <template #error="{ error }">
+        <p>{{ error }}</p>
+        <button @click="resetError(error)">Clear Error</button>
+      </template>
+    </NuxtErrorBoundary>
   </div>
 
 </template>
--- pages/users/list.vue
+++ pages/users/list.vue
@@ -1,11 +1,15 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  <h1>User List</h1>
+  <div>
+    <h1>에러 핸들링</h1>
+    <h2>Counter</h2>
+    <p>Count: {{ counter }}</p>
+    <div><button @click="inc">+</button></div>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
+<script setup>
+const counter = ref(0);
+const inc = () => {
+  throw createError('에러 발생');
+  counter.value++;
+};
+</script>
